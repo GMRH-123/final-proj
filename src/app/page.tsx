d6b2ae26a9aa5b846e3cbd303aac1aea33c38c34
RenzGabrielSalas <renzgabrielsalas@gmail.com>
2025-02-27 01:07:17 +0800

    User auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,40 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
+async function Images() {
+  
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) =>  desc(model.id),
   });
 
+  return(
+    
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default async function HomePage() {
+
   return (
     <main className="">
-        <div className="flex flex-wrap gap-4">
-          {/* {mockImages.map((image) => (
-            <div key={image.id} className="w-48">
-              <img src={image.url}/>
-            </div>
-          ))} */}
 
-          {images.map((image) => (
-            <div key={image.id} className="w-48">
-              <img src={image.url} />
-              <div>{image.name}</div>
-            </div>
-          ))}
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please Sign In Above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
 
-      </div>
     </main>
   );
 }
