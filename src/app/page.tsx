eea1f33352267852c55a5091c202f34ee78f7cd7
RenzGabrielSalas <renzgabrielsalas@gmail.com>
2025-02-27 08:47:02 +0800

    Use Next Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import Link from "next/link";
+import Image from "next/image";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
 
@@ -12,10 +12,16 @@ async function Images() {
 
   return(
     
-    <div className="flex flex-wrap justify-items-center gap-4">
+    <div className="flex flex-wrap justify-center gap-4 mt-4">
       {images.map((image) => (
-        <div key={image.id} className="w-48">
-          <img src={image.url} />
+        <div key={image.id} className="flex w-48 flex-col">
+          <Image 
+          src={image.url} 
+          style={{objectFit: "contain"}} 
+          width={200} 
+          height={200}
+          alt={image.name}
+          />
           <div>{image.name}</div>
         </div>
       ))}
