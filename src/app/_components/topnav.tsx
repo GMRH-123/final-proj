9a717dd61f589169ecf2eaf5bca33b0fa09d044c
RenzGabrielSalas <renzgabrielsalas@gmail.com>
2025-03-16 05:16:44 +0800

    "try on the fixation of vercel deployment"

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -6,7 +6,7 @@ export function TopNav() {
 
   return (
     <nav className="flex w-full items-center justify-between bg-neutral-900 py-3 px-8 text-xl font-semibold ">
-      <h1 className="text-neutral-300 text-xl tracking-wider">Gallery</h1>
+      <h1 className="text-neutral-300 text-xl tracking-wider">Pet Gallery</h1>
 
       <div className="flex flex-row items-center gap-4">
         <SignedOut>
