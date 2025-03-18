4debb09a08820727ef4122cabb9c0a2318f252b6
RenzGabrielSalas <renzgabrielsalas@gmail.com>
2025-03-18 23:29:02 +0800

    Update on the position of the components on Cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ async function Images() {
           <div className="flex pl-5 ml-1">
             {/* Display the user's avatar (currently using a placeholder image) */}
             <img
-              className="mt-1 mr-4 h-12 w-12 rounded-full object-cover shadow border border-gray-300"
+              className="mt-4 mr-4 h-12 w-12 rounded-full object-cover shadow border border-gray-300"
               src={image.userImg || "/path/to/fallback-image.jpg"}  // Use a fallback image
               alt="avatar"
             />
@@ -33,7 +33,7 @@ async function Images() {
                   {image.userName}
                 </p>
                 {/* Upload Time */}
-                <p className="pr-5 mr-5 italic text-gray-600 text-sm">
+                <p className="ml-5 mr-5 italic text-gray-600 text-sm">
                   {`Uploaded: ${timeAgo(new Date(image.createdAt))}`}
                 </p>
               </div>
